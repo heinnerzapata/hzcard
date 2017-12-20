fffbd3b922f704abf207f9cbbe6aaa230ea260f8
Your Name <you@example.com>
2017-12-19 21:04:29 -0500

    basic project structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import 'materialize-css/dist/css/materialize.min.css'
+import $ from 'jquery'
+import 'materialize-css/dist/js/materialize.min'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        HZ
+      </div>
+    );
+  }
+}
+
+export default App;
